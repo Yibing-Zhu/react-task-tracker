d37863b963d99db83b4fe6cea2532aebc3eb9244
Yibing-Zhu <91034728+Yibing-Zhu@users.noreply.github.com>
2022-03-21 15:16:06 -0400

    1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import Header from './components/Header'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+// function App() {
+
+//   return (
+//     <div className="App">
+//      <h1>Hello From React</h1>
+//     </div>
+//   );
+// }
+// class App extends React.Component{
+//   render(){
+//     return <h1>hello from class</h1>
+//   }
+// }
+const App = () =>{
+  return(
+    <div className='container'>
+      <Header />
     </div>
-  );
+  )
 }
-
 export default App;
